baeba93826bc3163d5b3dc9c0103cf3368e55cbe
Franck Giordano <gfranck2@myges.fr>
2025-12-11 12:15:26 +0100

    Refactor: Amélioration script (Help + formatage données)

--- monitor.ps1
+++ monitor.ps1
@@ -1,8 +1,41 @@
-Write-Host "--- MONITORING SYSTEME ---"
-Write-Host "CPU Usage:"
-Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select-Object Average
+param (
+    [switch]$Help
+)
 
-Write-Host "RAM Usage:"
-Get-CimInstance Win32_OperatingSystem | Select-Object FreePhysicalMemory
-Write-Host "Disk Space:"
-Get-PSDrive C | Select-Object Used, Free
+# help
+if ($Help) {
+    Write-Host "--------------------------------------------------" -ForegroundColor Green
+    Write-Host "AIDE - MONITOR.PS1" -ForegroundColor Green
+    Write-Host "--------------------------------------------------"
+    Write-Host "Description :"
+    Write-Host "  Ce script affiche l'état actuel des ressources système."
+    Write-Host ""
+    Write-Host "Utilisation :"
+    Write-Host "  .\monitor.ps1          Lancer le monitoring"
+    Write-Host "  .\monitor.ps1 -Help    Afficher cette aide"
+    Write-Host "--------------------------------------------------"
+    exit
+}
+
+Write-Host "--- MONITORING SYSTEME ---" -ForegroundColor Cyan
+
+# CPU
+$cpu = Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select-Object -ExpandProperty Average
+Write-Host "CPU Usage      : $cpu %"
+
+# 2. RAM 
+$os = Get-CimInstance Win32_OperatingSystem
+$totalRam = [math]::Round($os.TotalVisibleMemorySize / 1MB, 2)
+$freeRam  = [math]::Round($os.FreePhysicalMemory / 1MB, 2)
+$usedRam  = [math]::Round($totalRam - $freeRam, 2)
+$percRam  = [math]::Round(($usedRam / $totalRam) * 100, 1)
+
+Write-Host "RAM Usage      : Utilise $usedRam Go / Libre $freeRam Go (Total : $totalRam Go - $percRam%)"
+
+# 3 DISQUE 
+$disk = Get-PSDrive C
+$usedDisk = [math]::Round($disk.Used / 1GB, 2)
+$freeDisk = [math]::Round($disk.Free / 1GB, 2)
+$percDisk = [math]::Round(($disk.Used / ($disk.Used + $disk.Free)) * 100, 1)
+
+Write-Host "Disk Space (C) : Utilise $usedDisk Go / Libre $freeDisk Go (Occupation : $percDisk%)"
