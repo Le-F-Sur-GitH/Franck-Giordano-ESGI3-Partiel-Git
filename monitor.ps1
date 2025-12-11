145077609d92d5f3303834a948971f92acd9498e
Franck Giordano <gfranck2@myges.fr>
2025-12-11 12:01:09 +0100

    Fix: Remplacement commande RAM erronée par Get-CimInstance

--- monitor.ps1
+++ monitor.ps1
@@ -3,7 +3,6 @@ Write-Host "CPU Usage:"
 Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select-Object Average
 
 Write-Host "RAM Usage:"
-Get-MaRam -ErrorAction Stop
-
+Get-CimInstance Win32_OperatingSystem | Select-Object FreePhysicalMemory
 Write-Host "Disk Space:"
 Get-PSDrive C | Select-Object Used, Free
